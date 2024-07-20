96ca62efd8d65f73f1113a000357101df3f5862a
NhoetKyaiteKaungLayy <ohmyqueenmedusa20s@gmail.com>
2024-07-20 11:17:50 +0630

    Update id.py

--- ZenakuXMusic/plugins/tools/id.py
+++ ZenakuXMusic/plugins/tools/id.py
@@ -1,21 +1,101 @@
-from ZenakuXMusic import app
 from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.enums import ParseMode
 
-@app.on_message(filters.command("id"))
+from ZenakuXMusic import app
+
+
+@app.on_message(filters.command("me"))
 def ids(_, message):
     reply = message.reply_to_message
     if reply:
-        button = InlineKeyboardButton(" ᴄʟᴏsᴇ ", callback_data="close")
-        markup = InlineKeyboardMarkup([[button]])
         message.reply_text(
-            f"User {reply.from_user.first_name} ID is : {reply.from_user.id}",
-            reply_markup=markup
+            f"ʏᴏᴜʀ ɪᴅ: {message.from_user.id}\n{reply.from_user.first_name}'s ɪᴅ: {reply.from_user.id}\nᴄʜᴀᴛ ɪᴅ: {message.chat.id}"
         )
     else:
-        button = InlineKeyboardButton(" ᴄʟᴏsᴇ ", callback_data="close")
-        markup = InlineKeyboardMarkup([[button]])
-        message.reply(
-           f"ᴛʜɪs ɢʀᴏᴜᴩ's ɪᴅ ɪs: {message.chat.id}",
-           reply_markup=markup
-        )
+        message.reply(f"ʏᴏᴜʀ ɪᴅ: {message.from_user.id}\nᴄʜᴀᴛ ɪᴅ: {message.chat.id}")
+
+
+####
+
+
+@app.on_message(filters.command("id"))
+async def getid(client, message):
+    chat = message.chat
+    your_id = message.from_user.id
+    message_id = message.id
+    reply = message.reply_to_message
+
+    text = f"**[ᴍᴇssᴀɢᴇ ɪᴅ:]({message.link})** `{message_id}`\n"
+    text += f"**[ʏᴏᴜʀ ɪᴅ:](tg://user?id={your_id})** `{your_id}`\n"
+
+    if not message.command:
+        message.command = message.text.split()
+
+    if not message.command:
+        message.command = message.text.split()
+
+    if len(message.command) == 2:
+        try:
+            split = message.text.split(None, 1)[1].strip()
+            user_id = (await client.get_users(split)).id
+            text += f"**[ᴜsᴇʀ ɪᴅ:](tg://user?id={user_id})** `{user_id}`\n"
+
+        except Exception:
+            return await message.reply_text("ᴛʜɪs ᴜsᴇʀ ᴅᴏᴇsɴ'ᴛ ᴇxɪsᴛ.", quote=True)
+
+    text += f"**[ᴄʜᴀᴛ ɪᴅ:](https://t.me/{chat.username})** `{chat.id}`\n\n"
+
+    if (
+        not getattr(reply, "empty", True)
+        and not message.forward_from_chat
+        and not reply.sender_chat
+    ):
+        text += f"**[ʀᴇᴘʟɪᴇᴅ ᴍᴇssᴀɢᴇ ɪᴅ:]({reply.link})** `{reply.id}`\n"
+        text += f"**[ʀᴇᴘʟɪᴇᴅ ᴜsᴇʀ ɪᴅ:](tg://user?id={reply.from_user.id})** `{reply.from_user.id}`\n\n"
+
+    if reply and reply.forward_from_chat:
+        text += f"ᴛʜᴇ ғᴏʀᴡᴀʀᴅᴇᴅ ᴄʜᴀɴɴᴇʟ, {reply.forward_from_chat.title}, ʜᴀs ᴀɴ ɪᴅ ᴏғ `{reply.forward_from_chat.id}`\n\n"
+        print(reply.forward_from_chat)
+
+    if reply and reply.sender_chat:
+        text += f"ɪᴅ ᴏғ ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴄʜᴀᴛ/ᴄʜᴀɴɴᴇʟ, ɪs `{reply.sender_chat.id}`"
+        print(reply.sender_chat)
+
+    await message.reply_text(
+        text,
+        disable_web_page_preview=True,
+        parse_mode=ParseMode.DEFAULT,
+    )
+
+
+__MODULE__ = "Userid"
+__HELP__ = """
+## User ID Commands Help
+
+### 1. /me
+**Description:**
+Get your and replied user's IDs along with chat ID.
+
+**Usage:**
+/me [reply to a message]
+
+**Details:**
+- Retrieves your Telegram ID and the ID of the user you replied to.
+- Also provides the ID of the chat where the command is used.
+
+### 2. /id [username/ID]
+**Description:**
+Get message ID, your ID, user's ID (if provided), and chat ID.
+
+**Usage:**
+/id [username/ID]
+
+**Details:**
+- Retrieves the ID of the message, your Telegram ID, and the chat's ID.
+- If a username or ID is provided, also retrieves the ID of the specified user.
+- Additional information such as replied message ID and chat ID is provided if applicable.
+
+**Examples:**
+- `/id username`
+- `/id 123456789`
+"""
