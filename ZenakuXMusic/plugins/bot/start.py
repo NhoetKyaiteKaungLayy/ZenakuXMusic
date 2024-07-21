89a77b9502c7eab1db1c6ed9665b16d0216ebdbe
NhoetKyaiteKaungLayy <ohmyqueenmedusa20s@gmail.com>
2024-07-21 10:34:48 +0630

    Update start.py

--- ZenakuXMusic/plugins/bot/start.py
+++ ZenakuXMusic/plugins/bot/start.py
@@ -23,12 +23,6 @@ from ZenakuXMusic.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
 
-FLASH_STK = [
-"https://t.me/addstickers/Forbidden_15uun",
-"https://t.me/addstickers/Forbidden_10uun",
-
-]
-
 
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
@@ -38,8 +32,8 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            return await message.reply_sticker(
-                random.choice(FLASH_STK),
+            return await message.reply_video(
+                       video="https://te.legra.ph/file/fc75fef760c29e8ccdf8b.mp4",
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
